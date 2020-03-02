0743ecc2a2743d3ad2080c7c9bd35bc3ebc5ebb9
John M Daly <jmdaly@gmail.com>
2020-03-02 15:28:01 -0500

    Asw 16 while loop multi core (#1)
    * Add: Code to run a while loop on all available cores

--- apps/apex_ap/src/apex_ap.c
+++ apps/apex_ap/src/apex_ap.c
@@ -5,18 +5,55 @@
  **                                                               **
  *******************************************************************/
 
-#include "sdk.h"
 #include "platform_init.h"
+#include "sdk.h"
+
+#include "core/cortex_a9.h"
+#include "cpu_utility/cpu_utility.h"
+#include "utility/spinlock.h"
+
+/**
+ * @brief Global mutex controlling access to the console
+ */
+spinlock_t console_lock;
+
+/**
+ * @brief Example function that runs on each core. In practice, we will have
+ * different functions running on each core, but this shows how to concurrently
+ * execute code on each core.
+ */
+void say_hello_forever()
+{
+  uint32_t cpu_id = cpu_get_current();
+
+  while (1)
+  {
+    // Control access to the console
+    spinlock_lock(&console_lock, kSpinlockWaitForever);
+    printf("Apex while loop running on core %d.\n", cpu_id);
+    spinlock_unlock(&console_lock);
+  }
+}
 
 int main(void)
 {
-    platform_init();
+  platform_init();
 
-    printf("Starting the infinite while loop.\n");
-    while (1)
-    {
-        printf("Apex while loop running.\n");
-    }
+  printf("Starting an infinite while loop on all cores.\n");
 
-    return 0;
+  // Determine how many CPUs we have access to
+  int cpu_count = cpu_get_cores();
+
+  // Initialize mutexes
+  spinlock_init(&console_lock);
+
+  // Start all cpus and have them run say_hello_forever
+  for (int i = 1; i < cpu_count; i++)
+  {
+    cpu_start_secondary(i, &say_hello_forever, 0);
+  }
+
+  say_hello_forever();
+
+  return 0;
 }
